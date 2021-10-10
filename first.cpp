835a55391d5d05f216617664dcfbf642388e2497
sweety4535 <91516201+sweety4535@users.noreply.github.com>
2021-10-11 00:53:28 +0530

    adding two numbers in c++
    adding two number program in c++

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b; 
+    cout<<"Enter value of a"<<endl;
+    cin>>a;
+    
+    cout<<"Enter value of b"<<endl;
+    cin>>b;
+
+    int sum = a + b;
+    cout<<"the sum of two numbers :"<<sum<<endl;
+    return 0;
+}
